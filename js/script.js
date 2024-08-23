bc1fdafdb30a481f911e8e416ac7a87785ef1214
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-08-23 15:26:31 +0200

    first coommit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// controllo che veda il mio script.js
+console.log('Js ok');
+// ho fatto un file a parte per le mie funzioni faccio il controllo
+window.addEventListener("load", (event) => {
+    console.log("page is fully loaded");
+  });

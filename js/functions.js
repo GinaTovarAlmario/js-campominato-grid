013348b11d7380722e9eab58e8f6d971c0637409
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-08-23 16:54:48 +0200

    ho creato funzione per generare una cella da Js e le ho inserite in pagina

---
+++ js/functions.js
@@ -0,0 +1,17 @@
+// FUNZIONE PER CREARE CELLE
+/**
+ * 1- COME LA CHIAMO?
+ * 2-HO BISOGNO DI UN PARAMETRO?
+ * 3-HO BISOGNO DI RESTITUIRE QUALCOSA?
+ * 4- SE SI, DI CHE TIPO? 
+ */
+// DICHIARAZIONE DI FUNZIONE
+/**
+ * WITH THIS FUNCTION WE CREATE AN ELEMENT TYPE DIV WITH A CLASS NAMED CELL
+ * @returns <div class="cell"> </div>
+ */
+function createCell(){
+    const cell = document.createElement('div');
+    cell.className = 'cell';
+    return cell;
+}
--- js/script.js
+++ js/script.js
@@ -21,3 +21,21 @@ Aggiungere una select accanto al bottone di generazione, che fornisca una scelta
 - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
 - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;*/
+// FASE DI PREPARAZIONE
+// vado a recuperarmi la griglia del DOM
+const grid = document.getElementById('grid');
+// so che ho bisogno di una griglia 10x10
+// FASE DI RACCOLTA DATI
+const rows = 10;
+const cols = 10;
+const totalCells = rows * cols;
+// FASE DI ELABORAZIONE DATI
+// ora devo generare la mia griglia
+for(let i = 0; i < totalCells; i++){
+    // mi serve andare a creare una funzione che mi crei le celle
+    // invoco la funzione creata
+    const cell = createCell();
+    // devo inserire la cella in pagina
+    grid.appendChild(cell);
+}
+// FASE DI OUTPUT
